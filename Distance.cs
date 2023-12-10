777ba26f3b6c474e20aa49c288635c30cb43487e
HNBL <DerKudinov@gmail.com>
2023-12-10 20:14:26 +0300

    Add project files.

---
+++ Distance.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distance
+{
+    internal class Distance
+    {
+
+        int foot;
+        double inch;
+        public Distance(int foot, double inch)
+        {
+
+            this .foot = foot;  
+            this .inch = inch;
+
+
+        }
+
+        public override string ToString()
+        {
+            return Convert .ToString(this.foot) + " '-" + Convert.ToString(this.inch) + "\"";
+        }
+
+        public static Distance operator +(Distance dist1, Distance dist2)
+        {
+            int newfoot = dist1.foot + dist2.foot;
+            double newinch = dist1.inch + dist2.inch;
+            if (newinch >=12)
+            {
+
+                newfoot += (int)(newinch / 12);
+                newinch = newinch % 12;
+            }
+            return (new Distance(newfoot, newinch));
+        }
+        public static Distance operator -(Distance dist1, Distance dist2) 
+        {
+            double newinch;
+            int newfoot; 
+            if(dist1 .inch > dist2.inch) 
+            {
+            newfoot = dist1.foot -1-  dist2.foot;  
+                newinch =dist1 .inch +12 + dist2.inch;
+            }
+        else
+            {
+
+                newfoot = dist1.foot - dist2.foot;
+                newinch = dist1.inch - dist2.inch;
+
+            }
+           return (new Distance(newfoot, newinch));
+        }
+
+        public static bool operator >(Distance dist1, Distance dist2)
+        {
+            return (dist1.foot *12 +dist1.inch > dist2.foot*12+dist2.inch);
+
+        }
+        public static bool operator <(Distance dist1, Distance dist2)
+        {
+            return (dist1.foot * 12 + dist1.inch < dist2.foot * 12 + dist2.inch);
+
+        }
+        public static bool operator >=(Distance dist1, Distance dist2)
+        {
+            return (dist1.foot * 12 + dist1.inch >= dist2.foot * 12 + dist2.inch);
+
+        }
+        public static bool operator <=(Distance dist1, Distance dist2)
+        {
+            return (dist1.foot * 12 + dist1.inch <= dist2.foot * 12 + dist2.inch);
+
+        }
+
+        public static bool operator ==(Distance dist1, Distance dist2)
+        {
+            return (dist1.foot ==dist2.foot && dist1.inch == dist2.inch);
+
+        }
+        public static bool operator !=(Distance dist1, Distance dist2)
+        {
+            return (dist1.foot == dist2.foot && dist1.inch == dist2.inch);
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            Distance dist2 = (Distance)obj;
+            return(this.foot == dist2.foot && this.inch == dist2.inch);
+        }
+        public override int GetHashCode() 
+        {
+            return this.foot * 12 + (int)this.inch;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Distance
+{
+    class Program
+    {
+
+        static void Main(string[] args) 
+        {
+
+            Distance dist1 = new Distance(3, 5);
+            Distance dist2 = new Distance(5, 7);
+
+            Distance dist3 = dist1 + dist2;
+            Distance dist4 = dist1 - dist2;
+            Console.WriteLine("dist1= " + dist1.ToString());
+            Console.WriteLine("dist1= " + dist2.ToString());
+            Console.WriteLine("dist1= " + dist3.ToString());
+            Console.WriteLine("dist1= " + dist4.ToString());
+
+            if (dist3>dist4)
+            {
+                Console.WriteLine("dist3 больше dist4");
+
+            }
+            Console.ReadLine();
+        }
+
+
+    }
+
+
+}
